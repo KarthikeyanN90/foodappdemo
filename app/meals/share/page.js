e502ec7f4c3aa5e2929ec5fe7fd93e3ab07237a2
KarthikeyanN90 <karthibe77@gmai.com>
2023-12-13 23:41:53 +0530

    Working with ShareMealPage

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,22 @@ import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+  const shareMeal = async (formdata) => {
+    "use server";
+
+    const Meal = {
+      title: formdata.get("title"),
+      summary: formdata.get("summary"),
+      instructions: formdata.get("instructions"),
+      image: formdata.get("image"),
+      creator: formdata.get("name"),
+      creator_email: formdata.get("email"),
+    };
+
+
+    
+  };
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +27,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +55,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker lable="Meal Image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,22 +1,44 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 const ImagePicker = ({ lable, name }) => {
   const ImageInputRef = useRef();
+
+  const [pickerImage, setPickerImage] = useState();
 
   const handleImageClick = () => {
     ImageInputRef.current.click();
   };
 
-  const handleImageChange=()=>{
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (!file){
+      setPickerImage(null);
+      return;
+    }
 
-  }
+    const filereader = new FileReader();
+
+    filereader.onload = () => {
+      setPickerImage(filereader.result);
+    };
+
+    filereader.readAsDataURL(file);
+  };
 
   return (
     <div className={classes.picker}>
       <label htmlFor={name}></label>
       <div className={classes.controls}>
+
+        <div className={classes.preview}>
+
+          {!pickerImage && <p>No Image Preview</p>}
+          {pickerImage && <Image src={pickerImage} alt=" The Image was selected by user" fill />}
+
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -25,6 +47,7 @@ const ImagePicker = ({ lable, name }) => {
           name={name}
           ref={ImageInputRef}
           onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
